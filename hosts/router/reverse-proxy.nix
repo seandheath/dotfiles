146b52ca9970de3e42586ef2cb36cf1b8a43eeb5
Sean Heath <se@nheath.com>
2022-03-30 12:56:24 -0400

    added headscale recommended reverse proxy config

--- hosts/router/reverse-proxy.nix
+++ hosts/router/reverse-proxy.nix
@@ -21,10 +21,40 @@
     virtualHosts."hs.nheath.com" = {
       enableACME = true;
       forceSSL = true;
-      locations."/" = {
-        proxyPass = "http://127.0.0.1:8080/";
-        proxyWebsockets = true;
+      locations = {
+        "/headscale." = {
+          extraConfig = ''
+            grpc_pass grpc://${config.services.headscale.settings.grpc_listen_addr};
+          '';
+          priority = 1;
+        };
+        "/metrics" = {
+          proxyPass = "http://127.0.0.1:${toString config.services.headscale.port}";
+          extraConfig = ''
+            allow 10.0.0.0/8;
+            allow 100.64.0.0/16;
+            deny all;
+          '';
+          priority = 2;
+        };
+        "/" = {
+          proxyPass = "http://127.0.0.1:${toString config.services.headscale.port}";
+          extraConfig = ''
+            keepalive_requests          100000;
+            keepalive_timeout           160s;
+            proxy_buffering             off;
+            proxy_connect_timeout       75;
+            proxy_ignore_client_abort   on;
+            proxy_read_timeout          900s;
+            proxy_send_timeout          600;
+            send_timeout                600;
+          '';
+          priority = 99;
+        };
       };
+      extraConfig = ''
+        access_log /var/log/nginx/${domain}.access.log;
+      '';
     };
   };
 }
