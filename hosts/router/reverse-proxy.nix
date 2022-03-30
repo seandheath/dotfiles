ea924bf2a831019056a36220bf16d6dce0a79e71
Sean Heath <se@nheath.com>
2022-03-30 12:08:23 -0400

    updated proxy config because it wasn't forwarding

--- hosts/router/reverse-proxy.nix
+++ hosts/router/reverse-proxy.nix
@@ -8,6 +8,8 @@
     enable = true;
     recommendedProxySettings = true;
     recommendedTlsSettings = true;
+    recommendedGzipSettinsg = true;
+    recommendedOptimisation = true;
     virtualHosts."nc.nheath.com" = {
       enableACME = true;
       forceSSL = true;
@@ -15,6 +17,10 @@
         proxyPass = "http://10.0.0.2:80";
         proxyWebsockets = true;
       };
+      extraConfig = ''
+        proxy_ssl_server_name on;
+        proxy_pass_header Authorization;
+      '';
     };
     virtualHosts."hs.nheath.com" = {
       enableACME = true;
@@ -23,6 +29,10 @@
         proxyPass = "http://127.0.0.1:8080";
         proxyWebsockets = true;
       };
+      extraConfig = ''
+        proxy_ssl_server_name on;
+        proxy_pass_header Authorization;
+      '';
     };
   };
 }
