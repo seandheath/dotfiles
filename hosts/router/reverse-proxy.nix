ebab87941fe601b243846d584a9a71d7d45cf691
Sean Heath <se@nheath.com>
2022-04-01 18:55:35 -0400

    increased size for nextcloud requests

--- hosts/router/reverse-proxy.nix
+++ hosts/router/reverse-proxy.nix
@@ -16,6 +16,9 @@
       locations."/" = {
         proxyPass = "http://10.0.0.2/";
         proxyWebsockets = true;
+        extraConfig = ''
+          client_max_body_size 4G;
+        '';
       };
     };
     virtualHosts."hs.nheath.com" = {
