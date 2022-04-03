3c395dc47548e21019e228caaf09f1f3a5016adb
Sean Heath <se@nheath.com>
2022-04-03 15:44:53 -0400

    added kodi

--- home-modules/server.nix
+++ home-modules/server.nix
@@ -2,4 +2,22 @@
   imports = [
     ./core.nix
   ];
+  programs.kodi = {
+    enable = true;
+    sources = {
+      video = {
+        default = "movies";
+        source = [
+          {
+            name = "movies";
+            path = "/data/movies";
+          }
+          {
+            name = "tv";
+            path = "/data/tv";
+          }
+        ];
+      };
+    };
+  };
 }
