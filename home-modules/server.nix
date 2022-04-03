89e03433f1192bdef27806ab447809f96f25623e
Sean Heath <se@nheath.com>
2022-04-03 15:47:17 -0400

    removed kodi stuff from server

--- home-modules/server.nix
+++ home-modules/server.nix
@@ -2,22 +2,4 @@
   imports = [
     ./core.nix
   ];
-  programs.kodi = {
-    enable = true;
-    sources = {
-      video = {
-        default = "movies";
-        source = [
-          {
-            name = "movies";
-            path = "/data/movies";
-          }
-          {
-            name = "tv";
-            path = "/data/tv";
-          }
-        ];
-      };
-    };
-  };
 }
