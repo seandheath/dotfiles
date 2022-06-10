b1d4db145f08fe47784d5c19c27694f82bf56196
Sean Heath <se@nheath.com>
2022-06-10 13:33:20 -0400

    trying another way to do rootless

--- modules/veloren-server.nix
+++ modules/veloren-server.nix
@@ -4,14 +4,13 @@
   networking.firewall.allowedTCPPorts = [
     14004
   ];
-  #users.groups.veloren = {};
-  #users.users.veloren = {
-    #isNormalUser = true;
-    #group = config.users.groups.veloren.name;
-    #home = "/home/veloren";
-  #};
+  users.groups.veloren = {};
+  users.users.veloren = {
+    isNormalUser = true;
+    group = config.users.groups.veloren.name;
+    home = "/home/veloren";
+  };
   virtualisation.oci-containers.containers.veloren-server = {
-    #user = "veloren:veloren";
     image = "registry.gitlab.com/veloren/veloren/server-cli:nightly";
     autoStart = true;
     ports = [
@@ -25,4 +24,5 @@
     };
     extraOptions = [ "--userns=keep-id" ];
   };
+  systemd.services.podman-veloren-server.serviceConfig.User = "veloren";
 }
