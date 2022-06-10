d17dce0d612d871acf825ca1cf7b63a7469dc0aa
Sean Heath <se@nheath.com>
2022-06-10 13:03:34 -0400

    created separate veloren group

--- modules/veloren-server.nix
+++ modules/veloren-server.nix
@@ -1,20 +1,28 @@
 
 { config, pkgs, ... }:
 {
+  users.groups.veloren = {};
+  users.users.veloren = {
+    isNormalUser = true;
+    group = config.users.groups.veloren.name;
+    home = "/home/veloren";
+  };
   virtualisation.oci-containers.containers.veloren-game-server-master = {
+    user = "veloren:veloren";
     image = "registry.gitlab.com/veloren/veloren/server-cli:nightly";
     autoStart = true;
     ports = [
       "14004:14004"
     ];
     volumes = [
-      "/home/user/veloren:/opt/veloren"
+      "/home/veloren:/opt/veloren"
     ];
     environment = {
       RUST_LOG = "debug,common::net=info";
     };
   };
   virtualisation.oci-containers.containers.watchtower = {
+    user = "veloren:veloren";
     image = "containrrr/watchtower:latest";
     autoStart = true;
     volumes = [
