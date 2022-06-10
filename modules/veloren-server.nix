ee1a2eabbafd6927a59fe0bd5f97c8a82c4fbf1c
Sean Heath <se@nheath.com>
2022-06-10 13:14:04 -0400

    modified veloren server layout

--- modules/veloren-server.nix
+++ modules/veloren-server.nix
@@ -1,13 +1,16 @@
 
 { config, pkgs, ... }:
 {
+  networking.firewall.allowedTCPPorts = [
+    14004
+  ];
   users.groups.veloren = {};
   users.users.veloren = {
     isNormalUser = true;
     group = config.users.groups.veloren.name;
     home = "/home/veloren";
   };
-  virtualisation.oci-containers.containers.veloren-game-server-master = {
+  virtualisation.oci-containers.containers.veloren-server = {
     user = "veloren:veloren";
     image = "registry.gitlab.com/veloren/veloren/server-cli:nightly";
     autoStart = true;
@@ -21,7 +24,4 @@
       RUST_LOG = "debug,common::net=info";
     };
   };
-  networking.firewall.allowedTCPPorts = [
-    14004
-  ];
 }
