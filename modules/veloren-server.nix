fd67e8f51053c142a01e0c4919eec62ddbcd0db5
Sean Heath <se@nheath.com>
2022-06-10 13:51:19 -0400

    enabled stdin and tty for veloren server

--- modules/veloren-server.nix
+++ modules/veloren-server.nix
@@ -5,17 +5,27 @@
     14004
   ];
   virtualisation.oci-containers.backend = "docker";
-  virtualisation.oci-containers.containers.veloren-server = {
-    image = "registry.gitlab.com/veloren/veloren/server-cli:nightly";
-    autoStart = true;
-    ports = [
-      "14004:14004"
-    ];
-    volumes = [
-      "/home/user/veloren:/opt/veloren"
-    ];
-    environment = {
-      RUST_LOG = "debug,common::net=info";
-    };
+  virtualisation.oci-containers.containers = {
+    veloren = {
+      image = "registry.gitlab.com/veloren/veloren/server-cli:nightly";
+      autoStart = true;
+      ports = [
+        "14004:14004"
+      ];
+      volumes = [
+        "/home/user/veloren:/opt/veloren"
+      ];
+      environment = {
+        RUST_LOG = "debug,common::net=info";
+      };
+      extraOptions = [
+        "-i"
+        "-t"
+      ]
   };
+  watchtower = {
+    image = "containrrr/watchtower";
+    volumes = [ "/var/run/docker.sock:/var/run/docker.sock" ];
+    cmd = [ "--interval 30 --stop-timeout 130s --cleanup veloren"];
+  }
 }
