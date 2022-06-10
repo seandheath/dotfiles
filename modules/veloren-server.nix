287bdb0afdbaf953b2ad7c81f35036c534e2a368
Sean Heath <se@nheath.com>
2022-06-10 13:26:09 -0400

    turned off rootless podman

--- modules/veloren-server.nix
+++ modules/veloren-server.nix
@@ -4,21 +4,21 @@
   networking.firewall.allowedTCPPorts = [
     14004
   ];
-  users.groups.veloren = {};
-  users.users.veloren = {
-    isNormalUser = true;
-    group = config.users.groups.veloren.name;
-    home = "/home/veloren";
-  };
+  #users.groups.veloren = {};
+  #users.users.veloren = {
+    #isNormalUser = true;
+    #group = config.users.groups.veloren.name;
+    #home = "/home/veloren";
+  #};
   virtualisation.oci-containers.containers.veloren-server = {
-    user = "veloren:veloren";
+    #user = "veloren:veloren";
     image = "registry.gitlab.com/veloren/veloren/server-cli:nightly";
     autoStart = true;
     ports = [
       "14004:14004"
     ];
     volumes = [
-      "/home/veloren:/opt/veloren"
+      "/home/user/veloren:/opt/veloren"
     ];
     environment = {
       RUST_LOG = "debug,common::net=info";
