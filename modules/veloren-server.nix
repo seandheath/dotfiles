0c8ff510f7b28682b8412adc12c0b00d0846e5b9
Sean Heath <se@nheath.com>
2022-06-10 13:08:30 -0400

    removed watchtower

--- modules/veloren-server.nix
+++ modules/veloren-server.nix
@@ -21,17 +21,6 @@
       RUST_LOG = "debug,common::net=info";
     };
   };
-  virtualisation.oci-containers.containers.watchtower = {
-    user = "veloren:veloren";
-    image = "containrrr/watchtower:latest";
-    autoStart = true;
-    volumes = [
-      "/var/run/docker.sock:/var/run/docker.sock"
-    ];
-    cmd = [
-      "--interval 30 --stop-timeout 130s --cleanup veloren-game-server-master"
-    ];
-  };
   networking.firewall.allowedTCPPorts = [
     14004
   ];
