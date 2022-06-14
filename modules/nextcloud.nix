14c129432049e05e6db7729bae78c9ae48dd801c
Sean Heath <se@nheath.com>
2022-06-14 09:38:41 -0400

    testing pgsql with defaults

--- modules/nextcloud.nix
+++ modules/nextcloud.nix
@@ -25,11 +25,11 @@ in {
       overwriteProtocol = "https";
       adminuser = "sean";
       adminpassFile = adminpass;
-      dbuser = "nextcloud";
-      dbpassFile = dbpass;
+      #dbuser = "nextcloud";
+      #dbpassFile = dbpass;
       dbtype = "pgsql";
-      dbname = "nextcloud";
-      dbhost = "/run/postgresql";
+      #dbname = "nextcloud";
+      #dbhost = "/run/postgresql";
       extraTrustedDomains = [ "10.0.0.2" ];
     };
   };
