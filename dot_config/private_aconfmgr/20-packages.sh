9a26d58c4f63d82a6c7eeaab6f691d7439517037
Sean Heath <se@nheath.com>
2024-09-11 14:39:59 -0400

    Add .bashrc Remove .config/aconfmgr/30-yay.sh Change attributes of .config/aconfmgr/20-packages.sh

--- dot_config/private_aconfmgr/20-packages.sh
+++ dot_config/private_aconfmgr/20-packages.sh
@@ -28,3 +28,12 @@ AddPackage qt5-wayland
 AddPackage ripgrep
 AddPackage obsidian
 AddPackage chezmoi
+AddPackage openssh
+AddPackage age
+
+# AUR
+AddPackage --foreign aconfmgr-git # A configuration manager for Arch Linux
+AddPackage --foreign mkinitcpio-firmware # Optional firmware for the default linux kernel
+AddPackage --foreign yay # Yet another yogurt. Pacman wrapper and AUR helper written in go.
+AddPackage --foreign yay-debug # Detached debugging symbols for yay
+
--- dot_config/private_aconfmgr/30-yay.sh
+++
@@ -1,5 +0,0 @@
-AddPackage --foreign aconfmgr-git # A configuration manager for Arch Linux
-AddPackage --foreign mkinitcpio-firmware # Optional firmware for the default linux kernel
-AddPackage --foreign yay # Yet another yogurt. Pacman wrapper and AUR helper written in go.
-AddPackage --foreign yay-debug # Detached debugging symbols for yay
-
