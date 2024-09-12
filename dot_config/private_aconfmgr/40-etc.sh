b60b9cdf3b1514f17860de7ccf6be20cad661fb0
Sean Heath <se@nheath.com>
2024-09-12 11:38:49 -0400

    Update .config/aconfmgr/40-etc.sh

--- dot_config/private_aconfmgr/40-etc.sh
+++ dot_config/private_aconfmgr/40-etc.sh
@@ -3,6 +3,5 @@ CopyFile /etc/locale.gen
 CopyFile /etc/mkinitcpio.conf
 CopyFile /etc/pacman.conf
 CopyFile /etc/vconsole.conf
-CopyFile /etc/xdg/reflector/reflector.conf
 CreateLink /etc/localtime /usr/share/zoneinfo/America/New_York
 CreateLink /etc/os-release ../usr/lib/os-release
