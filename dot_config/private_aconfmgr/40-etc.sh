dde92165c16198aac8dd409d24c0510df1725aaf
Sean Heath <se@nheath.com>
2024-09-12 10:34:29 -0400

    Update .config/aconfmgr/40-etc.sh

--- dot_config/private_aconfmgr/40-etc.sh
+++ dot_config/private_aconfmgr/40-etc.sh
@@ -4,11 +4,12 @@ CopyFile /etc/fstab
 CopyFile /etc/hostname
 CopyFile /etc/locale.conf
 CopyFile /etc/locale.gen
-CreateLink /etc/localtime /usr/share/zoneinfo/America/New_York
 CopyFile /etc/machine-id 444
 CopyFile /etc/mkinitcpio.conf
 CopyFile /etc/mkinitcpio.d/linux.preset
-CreateLink /etc/os-release ../usr/lib/os-release
 CopyFile /etc/pacman.conf
 CopyFile /etc/shells
 CopyFile /etc/vconsole.conf
+CreateLink /etc/localtime /usr/share/zoneinfo/America/New_York
+CreateLink /etc/os-release ../usr/lib/os-release
+CreateLink /usr/lib/vmware/view/pkcs11/libopenscpkcs11.so /usr/lib64/pkcs11/opensc-pkcs11.so 
