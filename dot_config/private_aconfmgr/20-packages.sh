a71b679a42192671e37cc688e56862f7d4ccc8ca
Sean Heath <se@nheath.com>
2024-09-12 10:24:46 -0400

    Update .config/aconfmgr/20-packages.sh

--- dot_config/private_aconfmgr/20-packages.sh
+++ dot_config/private_aconfmgr/20-packages.sh
@@ -17,7 +17,6 @@ AddPackage xdg-desktop-portal-gnome
 AddPackage xdg-desktop-portal
 AddPackage xdg-user-dirs-gtk
 AddPackage power-profiles-daemon
-AddPackage nerd-fonts
 AddPackage reflector
 AddPackage fzf
 AddPackage cups
@@ -55,73 +54,6 @@ AddPackage libva-mesa-driver # Open-source VA-API drivers
 AddPackage mesa-vdpau # Open-source VDPAU drivers
 AddPackage vulkan-radeon # Open-source Vulkan driver for AMD GPUs
 AddPackage steam # Valve's digital software delivery system
-AddPackage otf-aurulent-nerd # Patched font Aurulent Sans Mono from nerd fonts library
-AddPackage otf-codenewroman-nerd # Patched font Code New Roman from nerd fonts library
-AddPackage otf-comicshanns-nerd # Patched font Comic Sans (Comic Shanns) from nerd fonts library
-AddPackage otf-commit-mono-nerd # Patched font Commit Mono from nerd fonts library
-AddPackage otf-droid-nerd # Patched font Droid Sans Mono from nerd fonts library
-AddPackage otf-firamono-nerd # Patched font Fira (Fura) Mono from nerd fonts library
-AddPackage otf-geist-mono-nerd # Patched font Geist Mono from nerd fonts library
-AddPackage otf-hasklig-nerd # Patched font Hasklig (Hasklug) from nerd fonts library
-AddPackage otf-hermit-nerd # Patched font Hermit from nerd fonts library
-AddPackage otf-monaspace-nerd # Patched font Monaspace (Monaspice) from nerd fonts library
-AddPackage otf-opendyslexic-nerd # Patched font OpenDyslexic from nerd fonts library
-AddPackage otf-overpass-nerd # Patched font Overpass from nerd fonts library
-AddPackage ttf-0xproto-nerd # Patched font 0xProto from nerd fonts library
-AddPackage ttf-3270-nerd # Patched font IBM 3270 from nerd fonts library
-AddPackage ttf-agave-nerd # Patched font Agave from nerd fonts library
-AddPackage ttf-anonymouspro-nerd # Patched font Anonymous Pro (Anonymice) from nerd fonts library
-AddPackage ttf-arimo-nerd # Patched font Arimo from nerd fonts library
-AddPackage ttf-bigblueterminal-nerd # Patched font Big Blue Terminal from nerd fonts library
-AddPackage ttf-bitstream-vera-mono-nerd # Patched font Bitstream Vera Mono from nerd fonts library
-AddPackage ttf-cascadia-code-nerd # Patched font Cascadia Code (Caskaydia) from nerd fonts library
-AddPackage ttf-cascadia-mono-nerd # Patched font Cascadia Mono (Caskaydia) from nerd fonts library
-AddPackage ttf-cousine-nerd # Patched font Cousine from nerd fonts library
-AddPackage ttf-d2coding-nerd # Patched font D2Coding from nerd fonts library
-AddPackage ttf-daddytime-mono-nerd # Patched font Daddy Time Mono from nerd fonts library
-AddPackage ttf-dejavu-nerd # Patched font Dejavu Sans Mono from nerd fonts library
-AddPackage ttf-envycoder-nerd # Patched font Envy Code R from nerd fonts library
-AddPackage ttf-fantasque-nerd # Patched font Fantasque Sans Mono from nerd fonts library
-AddPackage ttf-firacode-nerd # Patched font Fira (Fura) Code from nerd fonts library
-AddPackage ttf-go-nerd # Patched font Go Mono from nerd fonts library
-AddPackage ttf-gohu-nerd # Patched font Gohufont from nerd fonts library
-AddPackage ttf-hack-nerd # Patched font Hack from nerd fonts library
-AddPackage ttf-heavydata-nerd # Patched font Heavy Data from nerd fonts library
-AddPackage ttf-iawriter-nerd # Patched font iA Writer (iM Writing) from nerd fonts library
-AddPackage ttf-ibmplex-mono-nerd # Patched font IBM Plex Mono (Blex) from nerd fonts library
-AddPackage ttf-inconsolata-go-nerd # Patched font Inconsolata Go from nerd fonts library
-AddPackage ttf-inconsolata-lgc-nerd # Patched font Inconsolata LGC from nerd fonts library
-AddPackage ttf-inconsolata-nerd # Patched font Inconsolata from nerd fonts library
-AddPackage ttf-intone-nerd # Patched font Intone (Intel One) from nerd fonts library
-AddPackage ttf-iosevka-nerd # Patched font Iosevka from nerd fonts library
-AddPackage ttf-iosevkaterm-nerd # Patched font Iosevka Term from nerd fonts library
-AddPackage ttf-iosevkatermslab-nerd # Patched font Iosevka Term Slab from nerd fonts library
-AddPackage ttf-jetbrains-mono-nerd # Patched font JetBrains Mono from nerd fonts library
-AddPackage ttf-lekton-nerd # Patched font Lekton from nerd fonts library
-AddPackage ttf-liberation-mono-nerd # Patched font Liberation Mono from nerd fonts library
-AddPackage ttf-lilex-nerd # Patched font Lilex from nerd fonts library
-AddPackage ttf-martian-mono-nerd # Patched font Martian Mono from nerd fonts library
-AddPackage ttf-meslo-nerd # Patched font Meslo LG from nerd fonts library
-AddPackage ttf-monofur-nerd # Patched font Monofur from nerd fonts library
-AddPackage ttf-monoid-nerd # Patched font Monoid from nerd fonts library
-AddPackage ttf-mononoki-nerd # Patched font Mononoki from nerd fonts library
-AddPackage ttf-mplus-nerd # Patched font MPlus from nerd fonts library
-AddPackage ttf-nerd-fonts-symbols # High number of extra glyphs from popular 'iconic fonts'
-AddPackage ttf-nerd-fonts-symbols-mono # High number of extra glyphs from popular 'iconic fonts' (monospace)
-AddPackage ttf-noto-nerd # Patched font Noto from nerd fonts library
-AddPackage ttf-profont-nerd # Patched font ProFont from nerd fonts library
-AddPackage ttf-proggyclean-nerd # Patched font ProggyClean from nerd fonts library
-AddPackage ttf-recursive-nerd # Patched font Recursive Mono (Rec Mono) from nerd fonts library
-AddPackage ttf-roboto-mono-nerd # Patched font Roboto Mono from nerd fonts library
-AddPackage ttf-sharetech-mono-nerd # Patched font Share Tech Mono (Shure Tech Mono) from nerd fonts library
-AddPackage ttf-sourcecodepro-nerd # Patched font Source Code Pro from nerd fonts library
-AddPackage ttf-space-mono-nerd # Patched font Space Mono from nerd fonts library
-AddPackage ttf-terminus-nerd # Patched font Terminus (Terminess) from nerd fonts library
-AddPackage ttf-tinos-nerd # Patched font Tinos from nerd fonts library
-AddPackage ttf-ubuntu-mono-nerd # Patched font Ubuntu Mono from nerd fonts library
-AddPackage ttf-ubuntu-nerd # Patched font Ubuntu from nerd fonts library
-AddPackage ttf-victor-mono-nerd # Patched font Victor Mono from nerd fonts library
-AddPackage ttf-zed-mono-nerd # Patched font Zed Mono from nerd fonts library
 
 # AUR
 AddPackage --foreign aconfmgr-git # A configuration manager for Arch Linux
