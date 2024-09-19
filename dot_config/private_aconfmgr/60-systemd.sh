06886ff9d4175fe40c20ab382750bebb2c28a4df
Sean Heath <se@nheath.com>
2024-09-19 10:23:08 -0400

    Update .config/aconfmgr/60-systemd.sh

--- dot_config/private_aconfmgr/60-systemd.sh
+++ dot_config/private_aconfmgr/60-systemd.sh
@@ -1,10 +1,14 @@
+CopyFile /etc/systemd/logind.conf
 CreateLink /etc/systemd/system/bluetooth.target.wants/bluetooth.service /usr/lib/systemd/system/bluetooth.service
 CreateLink /etc/systemd/system/dbus-org.bluez.service /usr/lib/systemd/system/bluetooth.service
 CreateLink /etc/systemd/system/dbus-org.freedesktop.nm-dispatcher.service /usr/lib/systemd/system/NetworkManager-dispatcher.service
 CreateLink /etc/systemd/system/display-manager.service /usr/lib/systemd/system/gdm.service
 CreateLink /etc/systemd/system/getty.target.wants/getty@tty1.service /usr/lib/systemd/system/getty@.service
 CreateLink /etc/systemd/system/graphical.target.wants/power-profiles-daemon.service /usr/lib/systemd/system/power-profiles-daemon.service
+CreateLink /etc/systemd/system/hibernate.target /dev/null
+CreateLink /etc/systemd/system/hybrid-sleep.target /dev/null
 CreateLink /etc/systemd/system/mullvad-daemon.service.wants/mullvad-early-boot-blocking.service /usr/lib/systemd/system/mullvad-early-boot-blocking.service
+CreateLink /etc/systemd/system/multi-user.target.wants/auto-cpufreq.service /usr/lib/systemd/system/auto-cpufreq.service
 CreateLink /etc/systemd/system/multi-user.target.wants/cups.path /usr/lib/systemd/system/cups.path
 CreateLink /etc/systemd/system/multi-user.target.wants/cups.service /usr/lib/systemd/system/cups.service
 CreateLink /etc/systemd/system/multi-user.target.wants/libvirtd.service /usr/lib/systemd/system/libvirtd.service
@@ -13,7 +17,10 @@ CreateLink /etc/systemd/system/multi-user.target.wants/NetworkManager.service /u
 CreateLink /etc/systemd/system/multi-user.target.wants/reflector.service /usr/lib/systemd/system/reflector.service
 CreateLink /etc/systemd/system/multi-user.target.wants/remote-fs.target /usr/lib/systemd/system/remote-fs.target
 CreateLink /etc/systemd/system/network-online.target.wants/NetworkManager-wait-online.service /usr/lib/systemd/system/NetworkManager-wait-online.service
+CreateLink /etc/systemd/system/power-profiles-daemon.service /dev/null
 CreateLink /etc/systemd/system/printer.target.wants/cups.service /usr/lib/systemd/system/cups.service
+CreateLink /etc/systemd/system/sleep.target /dev/null
+CreateLink /etc/systemd/system/suspend.target /dev/null
 CreateLink /etc/systemd/system/sysinit.target.wants/systemd-boot-update.service /usr/lib/systemd/system/systemd-boot-update.service
 CreateLink /etc/systemd/user/default.target.wants/xdg-user-dirs-update.service /usr/lib/systemd/user/xdg-user-dirs-update.service
 CreateLink /etc/systemd/user/pipewire.service.wants/wireplumber.service /usr/lib/systemd/user/wireplumber.service
