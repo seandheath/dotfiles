9355c7a5dcb82e02ac73edddf9dec3dad2535b79
Sean Heath <se@nheath.com>
2024-09-19 10:32:12 -0400

    Remove .config/aconfmgr/sort.sh

--- dot_config/private_aconfmgr/executable_sort.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sort -o 10-ignores.sh 10-ignores.sh
-sort -o 20-packages.sh 20-packages.sh
-sort -o 40-etc.sh 40-etc.sh
-sort -o 50-usr.sh 50-usr.sh
-sort -o 60-systemd.sh 60-systemd.sh
