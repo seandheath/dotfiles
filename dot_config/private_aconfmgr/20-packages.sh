c88d06c3b7265e9da3a1689cba648dcdeb6ca733
Sean Heath <se@nheath.com>
2024-09-19 10:20:36 -0400

    Update .config/aconfmgr/20-packages.sh Update .config/aconfmgr/60-systemd.sh Add .config/aconfmgr/sort.sh

--- dot_config/private_aconfmgr/20-packages.sh
+++ dot_config/private_aconfmgr/20-packages.sh
@@ -1,87 +1,85 @@
-AddPackageGroup nerd-fonts
-AddPackage profile-sync-daemon
-AddPackage wget
-AddPackage nautilus
-AddPackage patchelf
-AddPackage ccid
-AddPackage rustup
-AddPackage swtpm
+AddPackage age
+AddPackage amd-ucode # Microcode update image for AMD CPUs
+AddPackage base-devel # Basic tools to build Arch Linux packages
+AddPackage base # Minimal package set to define a basic Arch Linux installation
 AddPackage bash-completion
-AddPackage libreoffice-fresh
-AddPackage texlive-xetex
-AddPackage xournalpp
-AddPackage wireshark-qt
-AddPackage opensc
-AddPackage virt-manager
 AddPackage btop
+AddPackage btrfs-progs # Btrfs filesystem utilities
+AddPackage ccid
+AddPackage chezmoi
+AddPackage code
+AddPackage cups
+AddPackage firefox
+AddPackage --foreign aconfmgr-git # A configuration manager for Arch Linux
+AddPackage --foreign code-marketplace
+AddPackage --foreign gnome-shell-extension-gtile
+AddPackage --foreign google-chrome
+AddPackage --foreign mkinitcpio-firmware # Optional firmware for the default linux kernel
+AddPackage --foreign mullvad-vpn-bin
+AddPackage --foreign pandoc-bin
+AddPackage --foreign vmware-horizon-client
+AddPackage --foreign vmware-horizon-smartcard
+AddPackage --foreign vmware-horizon-usb
+AddPackage --foreign yay-debug # Detached debugging symbols for yay
+AddPackage --foreign yay # Yet another yogurt. Pacman wrapper and AUR helper written in go.
+AddPackage fzf
 AddPackage gdm
-AddPackage nodejs
-AddPackage npm
-AddPackage typescript
-AddPackage gnome-shell
-AddPackage gnome-firmware
-AddPackage gnome-keyring
+AddPackage git # the fast distributed version control system
 AddPackage gnome-backgrounds
 AddPackage gnome-control-center
+AddPackage gnome-firmware
+AddPackage gnome-keyring
+AddPackage gnome-shell
 AddPackage gnome-shell-extension-appindicator
-AddPackage networkmanager
-AddPackage libnotify
-AddPackage libcanberra
-AddPackage libpulse
-AddPackage pulse-native-provider
-AddPackage xdg-desktop-portal-gnome
-AddPackage xdg-desktop-portal
-AddPackage xdg-user-dirs-gtk
-AddPackage power-profiles-daemon
-AddPackage reflector
-AddPackage fzf
-AddPackage cups
-AddPackage amd-ucode # Microcode update image for AMD CPUs
-AddPackage base # Minimal package set to define a basic Arch Linux installation
-AddPackage base-devel # Basic tools to build Arch Linux packages
-AddPackage btrfs-progs # Btrfs filesystem utilities
-AddPackage git # the fast distributed version control system
-AddPackage less # A terminal based program for viewing text files
-AddPackage linux # The Linux kernel and modules
-AddPackage linux-firmware # Firmware files for Linux
-AddPackage neovim # Fork of Vim aiming to improve user experience, plugins, and GUIs
-AddPackage networkmanager # Network connection manager and user applications
-AddPackage sudo # Give certain users the ability to run some commands as root
-AddPackage tree # A directory listing program displaying a depth indented list of files
-AddPackage vim # Vi Improved, a highly configurable, improved version of the vi text editor
-AddPackage which # A utility to show the full path of commands
 AddPackage go
-AddPackage firefox
-AddPackage kitty
-AddPackage otf-font-awesome
+AddPackageGroup nerd-fonts
 AddPackage keepassxc
-AddPackage qt5-wayland
-AddPackage ripgrep
-AddPackage obsidian
-AddPackage chezmoi
-AddPackage openssh
-AddPackage age
-AddPackage code
+AddPackage kitty
+AddPackage less # A terminal based program for viewing text files
 AddPackage lib32-libva-mesa-driver # Open-source VA-API drivers - 32-bit
 AddPackage lib32-mesa # Open-source OpenGL drivers - 32-bit
 AddPackage lib32-mesa-vdpau # Open-source VDPAU drivers - 32-bit
 AddPackage lib32-vulkan-radeon # Open-source Vulkan driver for AMD GPUs - 32-bit
+AddPackage libcanberra
+AddPackage libnotify
+AddPackage libpulse
+AddPackage libreoffice-fresh
 AddPackage libva-mesa-driver # Open-source VA-API drivers
+AddPackage linux-firmware # Firmware files for Linux
+AddPackage linux # The Linux kernel and modules
 AddPackage mesa-vdpau # Open-source VDPAU drivers
-AddPackage vulkan-radeon # Open-source Vulkan driver for AMD GPUs
-AddPackage steam # Valve's digital software delivery system
+AddPackage nautilus
+AddPackage neovim # Fork of Vim aiming to improve user experience, plugins, and GUIs
+AddPackage networkmanager
+AddPackage networkmanager # Network connection manager and user applications
+AddPackage nodejs
+AddPackage npm
+AddPackage obsidian
+AddPackage opensc
+AddPackage openssh
+AddPackage otf-font-awesome
+AddPackage patchelf
+AddPackage power-profiles-daemon
+AddPackage profile-sync-daemon
+AddPackage pulse-native-provider
+AddPackage qt5-wayland
+AddPackage reflector
+AddPackage ripgrep
+AddPackage rustup
 AddPackage signal-desktop
-
-# AUR
-AddPackage --foreign aconfmgr-git # A configuration manager for Arch Linux
-AddPackage --foreign mkinitcpio-firmware # Optional firmware for the default linux kernel
-AddPackage --foreign yay # Yet another yogurt. Pacman wrapper and AUR helper written in go.
-AddPackage --foreign yay-debug # Detached debugging symbols for yay
-AddPackage --foreign google-chrome
-AddPackage --foreign gnome-shell-extension-gtile
-AddPackage --foreign vmware-horizon-client
-AddPackage --foreign vmware-horizon-smartcard
-AddPackage --foreign vmware-horizon-usb
-AddPackage --foreign pandoc-bin
-AddPackage --foreign mullvad-vpn-bin
-AddPackage --foreign code-marketplace
+AddPackage steam # Valve's digital software delivery system
+AddPackage sudo # Give certain users the ability to run some commands as root
+AddPackage swtpm
+AddPackage texlive-xetex
+AddPackage tree # A directory listing program displaying a depth indented list of files
+AddPackage typescript
+AddPackage vim # Vi Improved, a highly configurable, improved version of the vi text editor
+AddPackage virt-manager
+AddPackage vulkan-radeon # Open-source Vulkan driver for AMD GPUs
+AddPackage wget
+AddPackage which # A utility to show the full path of commands
+AddPackage wireshark-qt
+AddPackage xdg-desktop-portal
+AddPackage xdg-desktop-portal-gnome
+AddPackage xdg-user-dirs-gtk
+AddPackage xournalpp
